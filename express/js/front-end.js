df03c8e00aae4324e855e3ead8350d68b3739e37
Dayu Wang <dwang@stchas.edu>
2022-03-21 10:25:13 -0500

    "Export input" added

--- express/js/front-end.js
+++ express/js/front-end.js
@@ -1,6 +1,6 @@
 // Created by Dayu Wang (dwang@stchas.edu) on 2022-03-20
 
-// Last updated by Dayu Wang (dwang@stchas.edu) on 2022-03-20
+// Last updated by Dayu Wang (dwang@stchas.edu) on 2022-03-21
 
 
 /** Returns the current date in "yyyy-mm-dd" format.
@@ -14,6 +14,31 @@ function getDate() {
     date = `${date.getDate()}`;
     if (date.length === 1) { date = '0' + date; }
     return `${year}-${month}-${date}`;
+}
+
+function exportInputData() {
+    let startDate = document.getElementById("start-date").value;
+    let xhr = new XMLHttpRequest();
+    let url = "#";
+    xhr.open("POST", url, true);
+
+    // Set the request header i.e. which type of content you are sending
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    // Create a state change callback
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // Print received data from server
+            console.log(this.responseText);
+        }
+    };
+
+    // Converting JSON data to string
+    let data = JSON.stringify({ "start": startDate });
+    console.log(data);
+
+    // Sending data with the request
+    xhr.send(data);
 }
 
 document.addEventListener("DOMContentLoaded",function() {
--- server.js
+++ server.js
@@ -1,18 +1,52 @@
 // Created by Dayu Wang (dwang@stchas.edu) on 2022-03-20
 
-// Last updated by Dayu Wang (dwang@stchas.edu) on 2022-03-20
+// Last updated by Dayu Wang (dwang@stchas.edu) on 2022-03-21
 
+
+/** Returns the current date and time in "yyyy-mm-ddThhmm" format.
+    @returns: current date in "yyyy-mm-dd" format
+*/
+function getTime() {
+    let date = new Date();
+    let year = `${date.getFullYear()}`;
+    let month = `${date.getMonth() + 1}`;
+    if (month.length === 1) { month = '0' + month; }
+    let day = `${date.getDate()}`;
+    if (day.length === 1) { day = '0' + date; }
+    let hour = `${date.getHours()}`;
+    if (hour.length === 1) { hour = '0' + hour; }
+    let min = `${date.getMinutes()}`;
+    if (min.length === 1) { min = '0' + min; }
+    return `${year}-${month}-${day}T${hour}${min}`;
+}
 
 const http = require("http");
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
+const fs = require("fs");
 
 const app = express();
-app.use(express.json());
 app.use(express.static("express"));
 
-app.use('/', function(req, res) {
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (_, res) => {
     res.sendFile(path.join(__dirname + "/express/index.html"));
+});
+
+app.post("/export", (req, res) => {
+    // Delete the old files in the directory.
+    const directory = path.join(__dirname + "/export_files");
+    fs.readdirSync(directory).forEach(file => {
+        fs.unlinkSync(directory + '/' + file);
+    });
+
+    const text = JSON.stringify(req.body);
+    const file = path.join(__dirname + "/export_files/Search_Input_-_" + getTime() + ".txt");
+    fs.writeFileSync(file, text.toString(), () => {});
+    res.download(file);
 });
 
 const server = http.createServer(app);

5a1d0b14d9a29e123397f04f0e6a1de99d302092
Dayu Wang <dwang@stchas.edu>
2022-03-20 21:45:24 -0500

    Initial committ

---
+++ server.js
@@ -0,0 +1,21 @@
+// Created by Dayu Wang (dwang@stchas.edu) on 2022-03-20
+
+// Last updated by Dayu Wang (dwang@stchas.edu) on 2022-03-20
+
+
+const http = require("http");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.use(express.json());
+app.use(express.static("express"));
+
+app.use('/', function(req, res) {
+    res.sendFile(path.join(__dirname + "/express/index.html"));
+});
+
+const server = http.createServer(app);
+const port = 3000;
+server.listen(port);
+console.debug('Server listening on port ' + port);
